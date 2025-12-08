4068858793dc22e1f55afd52c1be17b0c9ef2c61
Kasper Vikström <kasper.vik@hotmail.com>
2025-12-08 12:11:12 +0100

    Add passBomb function to handle bomb transfer between players

---
+++ src/services/rooms/passBomb.ts
@@ -0,0 +1,13 @@
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from "../../firebase";
+
+export async function passBomb(
+  roomId: string,
+  toPlayerId: string
+): Promise<void> {
+  const roomRef = doc(db, "rooms", roomId);
+
+  await updateDoc(roomRef, {
+    currentBombHolder: toPlayerId,
+  });
+}
