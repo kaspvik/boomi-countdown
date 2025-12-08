5ea1484a6ec9438c24e79371673b186081c55fdb
Kasper Vikström <kasper.vik@hotmail.com>
2025-12-08 12:11:20 +0100

    Export killPlayer and passBomb functions in rooms service

--- src/services/rooms/index.ts
+++ src/services/rooms/index.ts
@@ -2,4 +2,6 @@ export * from "./createRoom";
 export * from "./findRoomByCode";
 export * from "./generateRoomCode";
 export * from "./joinRoom";
+export * from "./killPlayer";
+export * from "./passBomb";
 export * from "./startGame";
