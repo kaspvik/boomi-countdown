122d61f68059a7a9d40bbe2e70d5412abbc02257
Kasper Vikström <kasper.vik@hotmail.com>
2025-12-26 19:12:09 +0100

    style: refine PixelFrame component styles and improve animation handling

--- src/layout/PixelFrame/PixelFrame.tsx
+++ src/layout/PixelFrame/PixelFrame.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import styles from "./PixelFrame.module.css";
-
 interface PixelFrameProps {
   children: React.ReactNode;
 }
-
 export const PixelFrame: React.FC<PixelFrameProps> = ({ children }) => {
   return (
     <section className={styles.pixelFrame}>
-      <div className={styles.content}>{children}</div>
+      {" "}
+      <div className={styles.content}>{children}</div>{" "}
     </section>
   );
 };
