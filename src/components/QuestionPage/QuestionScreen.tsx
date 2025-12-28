07fc628b55f08f6aab59d29e56ad8d2294fac044
Kasper Vikström <kasper.vik@hotmail.com>
2025-12-28 21:47:33 +0100

    style: update QuestionScreen layout for improved responsiveness and remove unused topBar component

--- src/components/QuestionPage/QuestionScreen.tsx
+++ src/components/QuestionPage/QuestionScreen.tsx
@@ -1,4 +1,3 @@
-import { PixelButton } from "../../layout/PixelButton/PixelButton";
 import { PixelFrame } from "../../layout/PixelFrame/PixelFrame";
 import type { Player, Room } from "../../types/game";
 import styles from "../QuestionPage/QuestionScreen.module.css";
@@ -20,18 +19,9 @@ export const QuestionScreen: React.FC<QuestionScreenProps> = ({
   currentPlayer,
   roomId,
   room,
-  onLeave,
 }) => {
   return (
     <main className={styles.main}>
-      <section className={styles.topBar}>
-        <div className={styles.leaveButton}>
-          <PixelButton onClick={onLeave} className="text-button">
-            Leave game
-          </PixelButton>
-        </div>
-      </section>
-
       <section className={styles.content}>
         <div className={styles.contentInner}>
           <PixelFrame>
