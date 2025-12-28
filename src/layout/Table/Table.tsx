bb00c5785874f02a77869b9886b46d80eab62f71
Kasper Vikström <kasper.vik@hotmail.com>
2025-12-28 14:10:44 +0100

    fix: update table image import path for correct asset loading

--- src/layout/Table/Table.tsx
+++ src/layout/Table/Table.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Table.module.css";
-import tableImg from "/public/table.png";
+import tableImg from "/table.png";
 
 interface TableProps {
   className?: string;
